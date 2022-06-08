a4230f41fdc48543f7100ca552d298d3742fff8d
DVSaltsev <DVSaltsev@gmail.com>
2022-06-08 14:37:20 +0500

    JS

--- js/feedback.js
+++ js/feedback.js
@@ -1,8 +1,16 @@
 
 const feedbackLink = document.querySelector(".footer__contacts-button");
-const feedbackPopup = document.querySelector(".feedback")
+const feedbackPopup = document.querySelector(".feedback");
+const feedbackClose = feedbackPopup.querySelector(".close");
+const feedbackLogin = feedbackPopup.querySelektor(".feedback__name-input");
 
 feedbackLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.add('feedback--show');
-});
+  // feedbackLogin.focus();
+})
+feedbackClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove('feedback--show');
+
+})
